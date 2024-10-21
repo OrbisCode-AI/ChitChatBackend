a870d7381b70db281d614001c95134c11167f305
shivatmax <awasthishiv0987@google.com>
2024-10-21 22:37:17 +0530

    chore: swagger

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from "@nestjs/platform-fastify";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 import { AppModule } from "@/app/app.module";
 
@@ -18,7 +19,17 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
   const port = configService.get<string>("PORT", "3000");
 
-  await app.listen(port, "0.0.0.0");
+  const config = new DocumentBuilder()
+    .setTitle("ChitChat Microservice")
+    .setDescription(
+      "This is the ChitChat microservice which is responsible for the AI chat functionality.",
+    )
+    .setVersion("1.0")
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, documentFactory);
+
+  await app.listen(process.env.PORT ?? 3000);
 
   const logger = app.get(Logger);
   logger.log(`App is ready and listening on port ${port} 🚀`);
